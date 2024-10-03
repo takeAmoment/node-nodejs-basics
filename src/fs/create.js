5474ef965bd93d9ffac12205a3c6b7510cc01a5d
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-03 23:01:55 +0200

    add fs create functionality

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { access, constants, writeFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const content = 'I am fresh and young';
+const successMessage = 'This file was created successfully';
+const errorMessage = 'This file already exists';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, 'files', 'fresh.txt');
+    
+    try {
+        await access(filePath, constants.F_OK);
+        throw new Error(errorMessage);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await writeFile(filePath, content);
+            console.log(successMessage);
+        } else {
+            console.log('Error:', err.message);
+        }
+    }
 };
 
 await create();
