d6193cb3633e2c204539dc52ac079061cfa10694
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-05 16:29:47 +0200

    feat: implement cli env functionality

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+const PREFIX = 'RSS_';
+const ABSENT_VARS_ERROR_MESSAGE = 'RSS variables do not exist';
+
 const parseEnv = () => {
-    // Write your code here 
+   const envVars = process.env;
+   const rssVars = Object.keys(envVars).filter((item) => item.startsWith(PREFIX));
+
+   if(rssVars.length === 0) {
+      console.log('Error:', ABSENT_VARS_ERROR_MESSAGE);
+      return;
+   }
+
+   const result = rssVars.map((item) => `${item}=${envVars[item]}`).join('; ');
+
+   console.log('Result:', result);
 };
 
 parseEnv();
