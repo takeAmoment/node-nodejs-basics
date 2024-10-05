d7a5f721f165e7d9893f87f6634d984b50035c61
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-05 12:42:25 +0200

    implement fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,27 @@
+import { readdir } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+import { checkIsExist } from '../helpers/checkIsExist.js';
+import { ERROR_MESSAGE, FILES_FOLDER_NAME } from '../variables/common.js';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = join(__dirname, FILES_FOLDER_NAME);
+    const isFolderExist = await checkIsExist(folderPath);
+
+    if(!isFolderExist){
+        return;
+    }
+
+    try {
+       const files = await readdir(folderPath);
+       console.log('Files array:', files);
+    } catch (error) {
+        console.log('Error:', ERROR_MESSAGE);
+    }
 };
 
 await list();
