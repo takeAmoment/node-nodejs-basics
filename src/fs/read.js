b71c83ba5de76607c247688fbe9ae4642563b851
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-05 12:53:24 +0200

    implement fs read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,29 @@
+import { readFile } from 'fs/promises';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+import { checkIsExist } from '../helpers/checkIsExist.js';
+import { ERROR_MESSAGE, FILES_FOLDER_NAME } from '../variables/common.js';
+
+const FILE_TO_READ_NAME = 'fileToRead.txt';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(__dirname, FILES_FOLDER_NAME, FILE_TO_READ_NAME);
+    const isFileExist = await checkIsExist(filePath);
+
+    if(!isFileExist) {
+        return;
+    }
+
+    try {
+        const fileContent = await readFile(filePath, 'utf8');
+        console.log('File content:', fileContent);
+    } catch (error) {
+        console.log('Error:', ERROR_MESSAGE);
+    }
 };
 
 await read();
