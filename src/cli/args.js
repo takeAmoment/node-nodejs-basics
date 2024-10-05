73885e317228dda61968634a55a565d8b9f56a64
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-05 16:44:42 +0200

    feat: implement cli args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,19 @@
+const PREFIX = '--';
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv;
+    let argsValues = [];
+    
+    args.forEach((item, index) => {
+        if(item.startsWith(PREFIX)) {
+            argsValues.push(`${item} is ${args[index + 1]}`);
+        }
+        return;
+    });
+
+    const result = argsValues.join(', ');
+
+    console.log(result);
 };
 
 parseArgs();
