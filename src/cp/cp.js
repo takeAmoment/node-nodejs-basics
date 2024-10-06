0e7be8827e9062cd1fa4d4072f64f0b3fba619cd
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-06 21:04:09 +0200

    feat: implement cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,24 @@
+import { spawn  } from 'child_process';
+import { stdin, stdout } from 'process';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+import { COMMON_ERROR_MESSAGE, FILES_FOLDER_NAME } from '../variables/common.js';
+
+const FILE_NAME = 'script.js';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+   const filePath = join(__dirname, FILES_FOLDER_NAME, FILE_NAME);
+
+   const child = spawn('node', [filePath, ...args]);
+
+   stdin.pipe(child.stdin);
+   child.stdout.pipe(stdout);
+
+   child.on('error', () => console.log(COMMON_ERROR_MESSAGE));
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([1, 2, 3]);
