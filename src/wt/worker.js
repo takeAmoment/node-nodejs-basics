4998c5897b092f74438f889c3c599c3bbf619431
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-06 18:54:02 +0200

    feat: implement workers

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,19 @@
+import { parentPort } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+
 // n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+
+    parentPort.on('message', (data) => {
+        const result = nthFibonacci(data);
+        parentPort.postMessage(result);
+    });
 };
 
 sendResult();
