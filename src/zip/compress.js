0a70ddf089bd69442fc14f2b28af741ac88da483
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-06 12:45:21 +0200

    feat: implement compress func

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,30 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline  } from 'stream';
+import { createGzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
+import { FILES_FOLDER_NAME, COMMON_ERROR_MESSAGE } from '../variables/common.js';
+
+const FILE_NAME = 'fileToCompress.txt';
+const ARCHIVE_FILE_NAME = 'fileToCompress.txt.gz'
+
+const __fileName = fileURLToPath(import.meta.url);
+const __dirname = dirname(__fileName);
+
 const compress = async () => {
-    // Write your code here 
+    const fileName = join(__dirname, FILES_FOLDER_NAME, FILE_NAME);
+    const archivedFile = join(__dirname, FILES_FOLDER_NAME, ARCHIVE_FILE_NAME);
+
+    const readableStream = createReadStream(fileName);
+    const writableStream = createWriteStream(archivedFile);
+    const gzip = createGzip();
+
+    pipeline(readableStream, gzip, writableStream, (error) => {
+        if(error) {
+            console.log(COMMON_ERROR_MESSAGE);
+        }
+    });
 };
 
 await compress();
