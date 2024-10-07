0d11facae6aed516a0fd1a185285640112d96077
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-07 17:52:49 +0200

    fix: remove prefix from props

--- src/cli/args.js
+++ src/cli/args.js
@@ -3,10 +3,11 @@ const PREFIX = '--';
 const parseArgs = () => {
     const args = process.argv;
     let argsValues = [];
+    const prefixExp = /^--/;
     
     args.forEach((item, index) => {
         if(item.startsWith(PREFIX)) {
-            argsValues.push(`${item} is ${args[index + 1]}`);
+            argsValues.push(`${item.replace(prefixExp, '')} is ${args[index + 1]}`);
         }
         return;
     });
