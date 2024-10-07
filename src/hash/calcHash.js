23144f0c57fd9b8544eaf1e50a99aafcbf86086d
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-07 17:59:57 +0200

    refactor: add changes to calcHash func

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,25 +1,26 @@
 import { createReadStream } from 'fs'
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 import { createHash } from 'crypto'
 
-import { FILES_FOLDER_NAME } from '../variables/common.js';
-import { COMMON_ERROR_MESSAGE } from '../variables/common.js';
+import { FILES_FOLDER_NAME } from '../variables/common.js'
 
-const FILE_NAME = 'fileToCalculateHashFor.txt';
+const FILE_NAME = 'fileToCalculateHashFor.txt'
 
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = dirname(__fileName);
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = dirname(__fileName)
 
 const calculateHash = async () => {
-    const fileName = join(__dirname, FILES_FOLDER_NAME, FILE_NAME);
+  const fileName = join(__dirname, FILES_FOLDER_NAME, FILE_NAME)
 
-    const hash = createHash('sha256');
-    const readableStream = createReadStream(fileName);
+  const hash = createHash('sha256');
+  const readableStream = createReadStream(fileName);
 
-    readableStream.on('error', () => { console.log('Error:', COMMON_ERROR_MESSAGE)});
-    readableStream.on('data', data => hash.update(data));
-    readableStream.on('end', () => console.log('Hash is', hash.digest('hex')));
-};
+  readableStream.on('error', () => {
+    throw new Error(ERROR_MESSAGE);
+  });
+  readableStream.on('data', (data) => hash.update(data));
+  readableStream.on('end', () => console.log('Hash is', hash.digest('hex')));
+}
 
-await calculateHash();
+await calculateHash()
