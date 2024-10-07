aca35e9d6307bce7b84dd481764af4837c811097
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-07 18:19:20 +0200

    refactor: refactor wt funcs

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,53 +1,56 @@
-import { cpus } from 'os';
-import { Worker } from 'worker_threads';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
-import { exit } from 'process';
+import { cpus } from 'os'
+import { Worker } from 'worker_threads'
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
+import { exit } from 'process'
 
-import { COMMON_ERROR_MESSAGE } from '../variables/common.js';
+import { COMMON_ERROR_MESSAGE } from '../variables/common.js'
 
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = dirname(__fileName);
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = dirname(__fileName)
 
-const START_WORKER_DATE_VALUE = 10;
-const WORKER_FILE_NAME = 'worker.js';
+const START_WORKER_DATE_VALUE = 10
+const WORKER_FILE_NAME = 'worker.js'
 
 const performCalculations = async () => {
-    const workerFilePath = join(__dirname, WORKER_FILE_NAME);
-    const cpusAmount = cpus().length;
-    const array =  new Array(cpusAmount).fill(1);
-    const promisesArray = [];
+  const workerFilePath = join(__dirname, WORKER_FILE_NAME)
+  const cpusAmount = cpus().length
+  const array = new Array(cpusAmount).fill(1)
+  const promisesArray = []
 
-    array.forEach((_, index) => {
-        const worker = new Worker(workerFilePath);
+  array.forEach((_, index) => {
+    const worker = new Worker(workerFilePath)
 
-        let promise = new Promise((resolve, reject) => {
-            worker.postMessage(START_WORKER_DATE_VALUE + index);
-            worker.on('message', (data) => resolve(data));
-            worker.on('error', () => reject(COMMON_ERROR_MESSAGE));
-        });
+    let promise = new Promise((resolve, reject) => {
+      worker.postMessage(START_WORKER_DATE_VALUE + index)
+      worker.on('message', (data) => resolve(data))
+      worker.on('error', () => reject(COMMON_ERROR_MESSAGE))
+    })
 
-        promisesArray.push(promise);
-    });
+    promisesArray.push(promise)
+  })
 
-    Promise.allSettled(promisesArray)
+  Promise.allSettled(promisesArray)
     .then((results) => {
-       const arr = results.map((res) => {
-        if (res.status ==='fulfilled') {
-            return {
-                status: "resolved",
-                data: res.value
-            }
+      const arr = results.map((res) => {
+        if (res.status === 'fulfilled') {
+          return {
+            status: 'resolved',
+            data: res.value
+          }
         } else {
-            return {
-                status: "error",
-                data: null
-            }
+          return {
+            status: 'error',
+            data: null
+          }
         }
-       });
-       console.log('Result:', arr);
-       exit(1);
-    }).catch(() => console.log(COMMON_ERROR_MESSAGE));
-};
+      })
+      console.log('Result:', arr)
+      exit(1)
+    })
+    .catch(() => {
+      throw new Error(COMMON_ERROR_MESSAGE)
+    })
+}
 
-await performCalculations();
+await performCalculations()
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,11 +1,5 @@
 import { parentPort } from 'worker_threads';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
 
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = dirname(__fileName);
-
-// n should be received from main thread
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
