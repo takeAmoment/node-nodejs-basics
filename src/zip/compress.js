9be05b10fe30ddeee6c1956ff85fe9cee1db232e
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-07 18:15:28 +0200

    fix: add check if files exists to zip funcs

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,27 +1,38 @@
-import { createReadStream, createWriteStream } from 'fs';
-import { pipeline  } from 'stream';
-import { createGzip } from 'zlib';
-import { fileURLToPath } from 'url';
-import { join, dirname } from 'path';
+import { createReadStream, createWriteStream } from 'fs'
+import { pipeline } from 'stream'
+import { createGzip } from 'zlib'
+import { fileURLToPath } from 'url'
+import { join, dirname } from 'path'
 
-import { FILES_FOLDER_NAME, COMMON_ERROR_MESSAGE, FILE_TO_COMPRESS_NAME, ARCHIVED_FILE_NAME } from '../variables/common.js';
+import {
+  FILES_FOLDER_NAME,
+  COMMON_ERROR_MESSAGE,
+  FILE_TO_COMPRESS_NAME,
+  ARCHIVED_FILE_NAME
+} from '../variables/common.js'
+import { checkIsExist } from '../helpers/checkIsExist.js'
 
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = dirname(__fileName);
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = dirname(__fileName)
 
 const compress = async () => {
-    const fileName = join(__dirname, FILES_FOLDER_NAME, FILE_TO_COMPRESS_NAME);
-    const archivedFile = join(__dirname, FILES_FOLDER_NAME, ARCHIVED_FILE_NAME);
+  const fileName = join(__dirname, FILES_FOLDER_NAME, FILE_TO_COMPRESS_NAME)
+  const archivedFile = join(__dirname, FILES_FOLDER_NAME, ARCHIVED_FILE_NAME)
+  const isFileExist = await checkIsExist(fileName)
 
-    const readableStream = createReadStream(fileName);
-    const writableStream = createWriteStream(archivedFile);
-    const gzip = createGzip();
+  if(!isFileExist) {
+    throw new Error(COMMON_ERROR_MESSAGE)
+  }
 
-    pipeline(readableStream, gzip, writableStream, (error) => {
-        if(error) {
-            console.log(COMMON_ERROR_MESSAGE);
-        }
-    });
-};
+  const readableStream = createReadStream(fileName)
+  const writableStream = createWriteStream(archivedFile)
+  const gzip = createGzip()
 
-await compress();
+  pipeline(readableStream, gzip, writableStream, (error) => {
+    if (error) {
+      throw new Error(COMMON_ERROR_MESSAGE)
+    }
+  })
+}
+
+await compress()
