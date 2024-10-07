513197cb8eeeb7cdc237cfe8db2a21a4cb08d75f
Aliaksandra <a.piakhota@andersenlab.com>
2024-10-07 18:21:14 +0200

    refactor: refactor cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,24 +1,27 @@
-import { spawn  } from 'child_process';
-import { stdin, stdout } from 'process';
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
+import { spawn } from 'child_process'
+import { stdin, stdout } from 'process'
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
 
-import { COMMON_ERROR_MESSAGE, FILES_FOLDER_NAME } from '../variables/common.js';
+import { COMMON_ERROR_MESSAGE, FILES_FOLDER_NAME } from '../variables/common.js'
 
-const FILE_NAME = 'script.js';
+const FILE_NAME = 'script.js'
 
-const __fileName = fileURLToPath(import.meta.url);
-const __dirname = dirname(__fileName);
+const __fileName = fileURLToPath(import.meta.url)
+const __dirname = dirname(__fileName)
 
 const spawnChildProcess = async (args) => {
-   const filePath = join(__dirname, FILES_FOLDER_NAME, FILE_NAME);
+  const filePath = join(__dirname, FILES_FOLDER_NAME, FILE_NAME)
 
-   const child = spawn('node', [filePath, ...args]);
+  const child = spawn('node', [filePath, ...args])
 
-   stdin.pipe(child.stdin);
-   child.stdout.pipe(stdout);
+  stdin.pipe(child.stdin)
+  child.stdout.pipe(stdout)
 
-   child.on('error', () => console.log(COMMON_ERROR_MESSAGE));
-};
+  child.on('error', () => {
+    throw new Error(COMMON_ERROR_MESSAGE)
+  })
+}
 
-spawnChildProcess([1, 2, 3]);
+spawnChildProcess([1, 2, 3])
+
